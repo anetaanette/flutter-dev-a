64f06bbd4f97c0c5afb8107802095aaa2b6aa4ff
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-07-12 00:12:23 +0200

    to be improved

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ console.log("kajdnsk");
 
 const search = document.querySelector(".element-2");
 let numbers = document.querySelector("input");
+const element1 = document.querySelector('.element-1');
+const element2 = document.querySelector('.element-2');
 
 search.addEventListener('click', searchNumber);
 
@@ -11,4 +13,30 @@ function searchNumber() {
     let array = [];
     array = numbersValue.replaceAll(',', ', ');
     console.log(array); 
+    element1.style.display = "none";
+    element2.style.display = "none";
+
+    var evens = [];
+    var odds = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2) {
+            evens.push(array[i]);
+        } else {
+            odds.push(array[i]);
+        }
+    }
+    let elen = evens.length;
+    let olen = odds.length;
+
+    if (elen > olen) {
+        let documentEvens = document.createTextNode(evens);
+       document.body.appendChild(documentEvens);
+       
+    } else {
+        let documentOdds = document.createTextNode(odds);
+        document.body.appendChild(documentOdds);
+    }
+
+    //let number = document.createTextNode(theNumber);
+
 }
