f538a8f7d7fc075c6bee13cfab429721276e06e9
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-07-15 17:38:14 +0200

    more changes

--- main.js
+++ main.js
@@ -4,24 +4,36 @@ const element1 = document.querySelector('.element-1');
 const element2 = document.querySelector('.element-2');
 
 search.addEventListener('click', findOutlier);
-let array = [];
-array = array.push(numbers.value);
-console.log(array);
 
 
-function findOutlier() {
-    // let array = [];
-   // array = push(numbers.value) //.split(', ').join('');
+//numbers = parseInt()
+ let numbersNumbers = numbers.value.split(',').map(function(item) {
+    return parseInt(item, 10);
+}) 
+console.log(typeof numbersNumbers);
+ function findOutlier() {
     element1.style.display = "none";
     element2.style.display = "none";
-    let evens = [];
+    let array = [];
+    array = array.push(numbersNumbers);
+    //array = array.push(numbers.split(', ').join(''));
+
+    console.log(numbers);
+    for (let i = 0; i < numbers.length; i++) {
+       // array[i] = array.push(numbers);
+       array = array.push(numbers.split(', ').join(''));
+
+    }
+
+   //array = array.push(numbers).split(', ').join('')
+    console.log(array);
+   /* let evens = [];
     let odds = [];
 
-
-  for (let i = 0; i < array.length; i++) {
+   for (let i = 0; i < array.length; i++) {
         if (array[i] % 2 === 0) {
             evens.push(array[i]);
-        } else {
+     } else {
             odds.push(array[i]);
         }   
     } 
@@ -30,6 +42,7 @@ function findOutlier() {
     let oLen = odds.length
     console.log(evens);
     console.log(odds);
+
     if (oLen > eLen) {
         let documentEvens = document.createTextNode(evens);
        document.body.appendChild(documentEvens);
@@ -37,6 +50,5 @@ function findOutlier() {
     } else {
         let documentOdds = document.createTextNode(odds);
         document.body.appendChild(documentOdds);
-    } 
+    } */
 }
-
