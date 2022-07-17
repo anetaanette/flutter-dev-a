91f9c24505d7fc449b5795922147459e69914a59
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-07-17 14:09:09 +0200

    solution without user's input

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 const search = document.querySelector(".element-2");
-let numbers = document.querySelector("input");
+let numbers = document.querySelector("input").value;
 const element1 = document.querySelector('.element-1');
 const element2 = document.querySelector('.element-2');
 
 search.addEventListener('click', findOutlier);
+let array = [];
 
+function findOutlier() {
+numbers = numbers.split(',').join(' ');
+function intoInt() {
+    numbers = parseInt(numbers);
+}
+intoInt();
 
-//numbers = parseInt()
- let numbersNumbers = numbers.value.split(',').map(function(item) {
-    return parseInt(item, 10);
-}) 
-console.log(typeof numbersNumbers);
- function findOutlier() {
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] = ' ') {
+        array = array.push(array[i+1]);
+    } else {
+        numbers = array.push(array[i]);
+    }
+}
+console.log(array);
+}
+
+/* function findOutlier() {
     element1.style.display = "none";
     element2.style.display = "none";
-    let array = [];
-    array = array.push(numbersNumbers);
-    //array = array.push(numbers.split(', ').join(''));
 
-    console.log(numbers);
-    for (let i = 0; i < numbers.length; i++) {
-       // array[i] = array.push(numbers);
-       array = array.push(numbers.split(', ').join(''));
-
-    }
-
-   //array = array.push(numbers).split(', ').join('')
-    console.log(array);
-   /* let evens = [];
+    let evens = [];
     let odds = [];
 
-   for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         if (array[i] % 2 === 0) {
             evens.push(array[i]);
      } else {
@@ -50,5 +50,60 @@ console.log(typeof numbersNumbers);
     } else {
         let documentOdds = document.createTextNode(odds);
         document.body.appendChild(documentOdds);
-    } */
+    } 
+
 }
+*/
+
+
+
+
+
+
+
+
+/* 
+// SOLUTION WITHOUT USER'S INPUT
+const search = document.querySelector(".element-2");
+const element1 = document.querySelector('.element-1');
+const element2 = document.querySelector('.element-2');
+
+
+search.addEventListener('click', findOutlier);
+
+
+let array = [2, 4, 0, 100, 4, 11, 2602, 36];
+array = [160, 3, 1719, 19, 11, 13, -21];
+console.log(array);
+
+
+function findOutlier() {
+    element1.style.display = "none";
+    element2.style.display = "none";
+
+    let evens = [];
+    let odds = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evens.push(array[i]);
+     } else {
+            odds.push(array[i]);
+        }   
+    } 
+
+    let eLen = evens.length;
+    let oLen = odds.length
+    console.log(evens);
+    console.log(odds);
+
+    if (oLen > eLen) {
+        let documentEvens = document.createTextNode(evens);
+       document.body.appendChild(documentEvens);
+       
+    } else {
+        let documentOdds = document.createTextNode(odds);
+        document.body.appendChild(documentOdds);
+    } 
+}
+*/ 
